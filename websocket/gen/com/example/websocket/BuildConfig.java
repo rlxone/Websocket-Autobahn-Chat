39e51e8aef7ee3a82a042930a2b8662c2ec2108b
I'm reflex <reflexblr@live.com>
2017-02-22 03:53:58 +0300

    delete gson from classpath

---
+++ websocket/gen/com/example/websocket/BuildConfig.java
@@ -0,0 +1,9 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.websocket;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+    public final static String WEBSOCKET_URL = "ws://192.168.1.87:5414";
+    //public final static String WEBSOCKET_URL = "ws://192.168.0.24:5414";
+    //public final static String WEBSOCKET_URL = "ws://192.168.1.3:81";
+}
