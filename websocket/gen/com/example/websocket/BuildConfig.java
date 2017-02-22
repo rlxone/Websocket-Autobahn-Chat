846a733197147ada5b5effdcaa3865d02ac9158c
I'm reflex <reflexblr@live.com>
2017-02-22 20:30:54 +0300

    localhost ws uri

--- websocket/gen/com/example/websocket/BuildConfig.java
+++ websocket/gen/com/example/websocket/BuildConfig.java
@@ -3,7 +3,7 @@ package com.example.websocket;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
-    public final static String WEBSOCKET_URL = "ws://192.168.1.87:5414";
+    //public final static String WEBSOCKET_URL = "ws://192.168.1.87:5414";
     //public final static String WEBSOCKET_URL = "ws://192.168.0.24:5414";
-    //public final static String WEBSOCKET_URL = "ws://192.168.1.3:81";
+    public final static String WEBSOCKET_URL = "ws://127.0.0.1:5414";
 }
